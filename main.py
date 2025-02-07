49913f46591e0eb25d1206fb1faf54a311811e93
Matte <matte.boni2101@gmail.com>
2025-02-07 12:46:03 +0100

    pt2

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
+import matplotlib.pyplot as plt
+import numpy as np
+from datasets import gaussians_dataset
+from utils import plot_2d_dataset
+from utils import plot_boundary
 
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from boosting import AdaBoostClassifier
+
+plt.ion()
+
+def main_adaboost():
+    """
+    Main function for testing Adaboost.
+    """
+
+    X_train, Y_train, X_test, Y_test = gaussians_dataset(2, [300, 400], [[1, 3], [-4, 8]], [[2, 3], [4, 1]])
+    # X_train, Y_train, X_test, Y_test = h_shaped_dataset()
+    # X_train, Y_train, X_test, Y_test = two_moon_dataset(n_samples=500, noise=0.2)
+
+    # visualize dataset
+    plot_2d_dataset(X_train, Y_train, 'Training')
+
+    # train model and predict
+    model = AdaBoostClassifier(n_learners=100)
+
+    model.fit(X_train, Y_train, verbose=True)
+    P = model.predict(X_test)
+
+    # visualize the boundary!
+    plot_boundary(X_train, Y_train, model)
+
+    # evaluate and print error
+    error = float(np.sum(P == Y_test)) / Y_test.size
+    print('Test set - Classification Accuracy: {}'.format(error))
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
+# entry point
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main_adaboost()
